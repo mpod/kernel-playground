98c52ebcd6d656bc3087ee2d506fb29aafed41fe
Matija Podravec <matija_podravec@fastmail.fm>
2016-04-16 14:24:16 +0100

    Updated license

--- bmp280/bmp280.c
+++ bmp280/bmp280.c
@@ -1,13 +1,24 @@
 /*
  * bmp280.c
  *
- * Copyright (c) 2016 Matija Podravec <matija_podravec@fastmail.fm>
+ * Copyright (C) 2016 Matija Podravec <matija_podravec@fastmail.fm>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This file is subject to the terms and conditions of version 2 of
- * the GNU General Public License.  See the file COPYING in the main
- * directory of this archive for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *
  * Driver for the Bosch BMP280 Digital Pressure Sensor 
+ *
  *
  */
 
